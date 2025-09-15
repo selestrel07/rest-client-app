4a50a670e3b43d4eb701176cdb467c25b66c9063
heresyhawkins <heresyhawkins@gmail.com>
2025-09-15 12:44:42 +0300

    feat: add validation for rus language

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -20,6 +20,8 @@ export const schema = yup
       )
       .matches(/[a-z]/, 'Password must contain a lowercase letter')
       .matches(/[A-Z]/, 'Password must contain an uppercase letter')
+      .matches(/[a-zа-я]/, 'Password must contain a lowercase letter')
+      .matches(/[A-ZА-Я]/, 'Password must contain an uppercase letter')
       .matches(/[0-9]/, 'Password must contain a number')
       .matches(/[!@#$%^&*]/, 'Password must contain a special character'),
     confirmPassword: yup
