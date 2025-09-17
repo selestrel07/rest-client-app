0da0e5f914e6e3c908d8ca4ea5866261a8dedc55
selestrel07 <selestrel07@gmail.com>
2025-09-17 14:21:21 +0200

    refactor: remove commented out code

--- src/states/uiSlice.ts
+++ src/states/uiSlice.ts
@@ -12,15 +12,6 @@ const initialState: UiState = {
   user: null,
 };
 
-// it's for checking authentificated user (fake user)
-// need delete it
-
-// const initialState: UiState = {
-//   locale: 'en',
-//   isAuthenticated: true,
-//   user: { uid: '123', email: 'test@example.com' },
-// };
-
 const uiSlice = createSlice({
   name: 'ui',
   initialState,
