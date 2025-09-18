6524f81d33e12ae94942134494dcf24b90e7ec4b
Isvaya <tasyana.pulatova@gmail.com>
2025-09-18 13:06:15 +0500

    feat: add promise in rest page

--- src/app/[locale]/rest/[method]/[[...requestpart]]/page.tsx
+++ src/app/[locale]/rest/[method]/[[...requestpart]]/page.tsx
@@ -1,14 +1,14 @@
 import { CodeGenerator } from '@components';
 import { decodeBase64, parseQuery } from '@utils/urlEncoding';
 
-export default function RestPage({
+export default async function RestPage({
   params,
   searchParams,
 }: {
-  params: { method: string; requestpart?: string[] };
+  params: Promise<{ method: string; requestpart?: string[] }>;
   searchParams: Record<string, string>;
 }) {
-  const { method, requestpart = [] } = params;
+  const { method, requestpart = [] } = await params;
 
   const [urlBase64, bodyBase64] = requestpart;
 
