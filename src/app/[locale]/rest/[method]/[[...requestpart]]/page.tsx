e75e91edd596488da2b1c5e4ded9036b59675a3d
heresyhawkins <heresyhawkins@gmail.com>
2025-09-21 18:30:56 +0300

    feat: add lazy loading

--- src/app/[locale]/rest/[method]/[[...requestpart]]/page.tsx
+++ src/app/[locale]/rest/[method]/[[...requestpart]]/page.tsx
@@ -1,7 +1,10 @@
 import dynamic from 'next/dynamic';
 
 const RestClientLazy = dynamic(
-  () => import('@components').then((mod) => mod.RestClient),
+  () =>
+    import('../../../../../components/RestClient/RestClient').then(
+      (mod) => mod.RestClient
+    ),
   {
     loading: () => (
       <p className="text-center p-4 text-violet-950">Loading REST client...</p>
